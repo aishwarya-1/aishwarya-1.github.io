d34c17b940819d16e45cb75e58eb06e7eb0bb7b1
aishwarya <aishwaryakalgudi@gmail.com>
2022-04-06 19:50:13 +0530

    add social media buttons

--- src/App.js
+++ src/App.js
@@ -10,14 +10,13 @@ function App() {
           <h4> <code>Hello, World! I am </code> </h4>
           <h2> Aishwarya Kumaraswamy</h2>  
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="socialbtns">
+          <ul>
+              <li><a href="https://twitter.com/TheTweetOfGod" class="fa fa-lg fa-twitter"></a></li>
+              <li><a href="https://github.com/aishwarya-1" class="fa fa-lg fa-github"></a></li>
+              <li><a href="https://www.linkedin.com/in/aishwarya-kumaraswamy-9721a617a/" class="fa fa-lg fa-linkedin"></a></li>
+          </ul>
+      </div>
       </header>
     </div>
   );
